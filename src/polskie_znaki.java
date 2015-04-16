e36b8ac6e2a36e3f3333398468e465abb415e63c
Anna <annasmo@student.agh.edu.pl>
2015-04-16 11:16:18 +0200

    utf8

--- src/polskie_znaki.java
+++ src/polskie_znaki.java
@@ -2,8 +2,8 @@
 public class polskie_znaki {
 
 	public static void main(String[] args) {
-		System.out.println("ZA¯Ó£Æ GÊŒL¥ JAÑ");
+		System.out.println("ZAÅ»Ã“ÅÄ† GÄ˜ÅšLÄ„ JAÅ¹Åƒ");
 		System.out.println("No i jak?");
-        System.out.println("koœæ nie¿gody");
+        System.out.println("koÅ›Ä‡ nieÅ¼gody");
     }
 }
