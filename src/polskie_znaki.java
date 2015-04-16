6f2ff3e5bf1ce979dfa6aaf871ce9069a850fed0
Anna <annasmo@student.agh.edu.pl>
2015-04-16 11:19:25 +0200

    utf8

--- src/polskie_znaki.java
+++ src/polskie_znaki.java
@@ -4,6 +4,5 @@ public class polskie_znaki {
 	public static void main(String[] args) {
 		System.out.println("ZAŻÓŁĆ GĘŚLĄ JAŹŃ");
 		System.out.println("No i jak?");
-        System.out.println("kość nieżgody");
     }
 }
